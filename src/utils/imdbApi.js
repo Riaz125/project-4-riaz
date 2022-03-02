46eee811ce98886d500791ff9e1576f322cd98f1
Riaz <riazzzz@gmail.com>
2022-03-01 22:00:39 -0800

    fixed api key vulnerability

--- src/utils/imdbApi.js
+++ src/utils/imdbApi.js
@@ -1,10 +1,14 @@
 
+
 export function getAllIMDB() {
+    console.log(process.env.REACT_APP_API_KEY)
     return fetch("https://ott-details.p.rapidapi.com/advancedsearch?start_year=2020&end_year=2021&min_imdb=6&max_imdb=7.8&genre=action&language=english&type=movie&sort=highestrated&page=1", {
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "ott-details.p.rapidapi.com",
-            "x-rapidapi-key": "59875b1b5cmsh8127719c2cf1825p17dfa9jsnf2e519a5c3ce"
+            "x-rapidapi-key": process.env.REACT_APP_API_KEY
+
+
         }
 
 	}).then(res => {
